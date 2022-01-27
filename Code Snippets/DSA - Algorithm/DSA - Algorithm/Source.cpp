89e583893eda7a58eb7386dce977f58d7fd94ddc
bstowemasters <benjamin.stowe-masters20@bathspa.ac.uk>
2022-01-27 12:05:23 +0000

    Commented Functional Code

--- Code Snippets/DSA - Algorithm/DSA - Algorithm/Source.cpp
+++ Code Snippets/DSA - Algorithm/DSA - Algorithm/Source.cpp
@@ -1,106 +1,73 @@
-// C++ program to check if there is exist a path between two vertices
-// of a graph.
 #include<iostream>
-#include <list>
+#include<climits>
 using namespace std;
 
-// This class represents a directed graph using adjacency list
-// representation
-class Graph
-{
-	int V; // No. of vertices
-	list<int>* adj; // Pointer to an array containing adjacency lists
-public:
-	Graph(int V); // Constructor
-	void addEdge(int v, int w); // function to add an edge to graph
-	bool isReachable(int s, int d);
-};
+#define v 8
 
-Graph::Graph(int V)
+int miniDist(int dist[], bool visited[]) // finding minimum distance
 {
-	this->V = V;
-	adj = new list<int>[V];
+    int min = INT_MAX, ind;              // sets the min value to infinite & Initialises index var.
+
+    for (int k = 0; k < v; k++)          // iterates through the verticies.
+    {
+        if (visited[k] == false && dist[k] <= min)  // Checks if the node hasn't been visited yet and if the current path is shorter than the minimum known path ( which is infinite on the initial iteration ).
+                                                    // This is used to find path length to each vertex.
+        {
+            min = dist[k];              // Sets the min value to the current index of min known distance. ( ready for comparison in the next iteration of the for loop )
+            ind = k;                    // Sets the index value to the current index of the iterator / for loop so this can be returned back as the shortest path index for this node/edge.
+        }
+    }
+    return ind;                         // Returns index for shortest identified path.
 }
 
-void Graph::addEdge(int v, int w)
+void DijkstraAlgo(int graph[v][v], int src) // adjacency matrix     // Outputs all of the shortest paths from the source node to every other node.
 {
-	adj[v].push_back(w); // Add w to v’s list.
+    int dist[v]; // array to store the minimum path length for each node.                           
+    bool visited[v];// Boolean array to store visited or unvisited for each node.
+
+
+    for (int k = 0; k < v; k++) // Iterator to run through each index of the arrays.
+    {
+        dist[k] = INT_MAX;  // Assumes the distance is infinite and assigns value to each index.      ( Initialisation )
+        visited[k] = false;        // False values assigned to all indexes to store unvisited for each node. ( Initialisation )
+    }
+
+    dist[src] = 0;   // Sets the distance of the source node to 0 as this is the only known path length.
+
+    for (int k = 0; k < v; k++) // Iterates through all of the indexes of the arrays.
+    {
+        int m = miniDist(dist, visited);   // Sets m to the calculated minimum distance from each node
+        visited[m] = true;                     // Sets the current node to explored
+        for (int k = 0; k < v; k++)
+        {
+            // updating the distance of neighbouring vertex
+            if (!visited[k] && graph[m][k] && dist[m] != INT_MAX && dist[m] + graph[m][k] < dist[k])   // Checks if the the current node has not yet been visited. Checks if the cost for this edge is less than the currently known shortest path
+                dist[k] = dist[m] + graph[m][k];                                                       // Updates the shortest known path from the source to the goal node, Adds previous total pathlength to newly discovered node.
+        }
+    }
+    cout << "Vertex\t\tDistance from source" << endl;        // Titles for output
+    for (int k = 0; k < v; k++)                             // Iterate through saved list.
+    {
+        char str = 65 + k;                                  // Translates numerical values into letters
+        cout << str << "\t\t\t" << dist[k] << endl;         // Outputs results.
+    }
 }
 
-// A BFS based function to check whether d is reachable from s.
-bool Graph::isReachable(int s, int d)
-{
-	// Base case
-	if (s == d)
-		return true;
-
-	// Mark all the vertices as not visited
-	bool* visited = new bool[V];
-	for (int i = 0; i < V; i++)
-		visited[i] = false;
-
-	// Create a queue for BFS
-	list<int> queue;
-
-	// Mark the current node as visited and enqueue it
-	visited[s] = true;
-	queue.push_back(s);
-
-	// it will be used to get all adjacent vertices of a vertex
-	list<int>::iterator i;
-
-	while (!queue.empty())
-	{
-		// Dequeue a vertex from queue and print it
-		s = queue.front();
-		queue.pop_front();
-
-		// Get all adjacent vertices of the dequeued vertex s
-		// If a adjacent has not been visited, then mark it visited
-		// and enqueue it
-		for (i = adj[s].begin(); i != adj[s].end(); ++i)
-		{
-			// If this adjacent node is the destination node, then
-			// return true
-			if (*i == d)
-				return true;
-
-			// Else, continue to do BFS
-			if (!visited[*i])
-			{
-				visited[*i] = true;
-				queue.push_back(*i);
-			}
-		}
-	}
-
-	// If BFS is complete without visiting d
-	return false;
-}
-
-// Driver program to test methods of graph class
 int main()
 {
-	// Create a graph given in the above diagram
-	Graph g(4);
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
-	g.addEdge(1, 2);
-	g.addEdge(2, 0);
-	g.addEdge(2, 3);
-	g.addEdge(3, 3);
+    int graph[v][v] = {
 
-	int u = 1, v = 3;
-	if (g.isReachable(u, v))
-		cout << "\n There is a path from " << u << " to " << v;
-	else
-		cout << "\n There is no path from " << u << " to " << v;
-
-	u = 3, v = 1;
-	if (g.isReachable(u, v))
-		cout << "\n There is a path from " << u << " to " << v;
-	else
-		cout << "\n There is no path from " << u << " to " << v;
-
-	return 0;
+        // Cost matrix showing edge cost from vertex [LEFT V] to - [TOP V]
+        //		A	B	C	D	 E	 F	 G	 H
+        /*A*/ {	0,  10, 0,  12,  0,	 0,  11, 4	},
+        /*B*/ { 0,  0,  0,	8,	 0,	 0,	 0,	 20	},
+        /*C*/ { 0,  0,  0,  17,	 8,  0,  13, 0	},
+        /*D*/ { 0,  0,  0,  0,	 0,  16, 24, 14	},
+        /*E*/ { 0,  0,  0,  0,   0,	 8,	 0,  0	},
+        /*F*/ { 0,  0,  0,  0,   0,  0,	 18, 2	},
+        /*G*/ { 0,  0,  0,  0,   11, 0,  0,	 3	},
+        /*H*/ { 0,  0,  10, 0,   5,	 0,  0,  0	}
+    };
+    DijkstraAlgo(graph, 0);         // Calls the algorithm funciton with the source node as a parameter.
+    return 0;
 }
--- Code Snippets/Final Algorithm/Final Algorithm/Source.cpp
+++ Code Snippets/Final Algorithm/Final Algorithm/Source.cpp
@@ -1,102 +1,78 @@
-#include<stdlib.h>
-#include<vector>
-#include<array>
 #include<iostream>
-#include<algorithm>
+#include<climits>
 using namespace std;
 
-#define V 8
+#define v 8
 
-void toInfinite(array <array<int, V>, V>& minKnownDist);
+int miniDist(int dist[], bool visited[]) // finding minimum distance
+{
+    int min = 10000, ind;              // sets the min value to infinite & Initialises index var.
 
-int main() {
-
-	int graph[V][V] = {
-
-		//		A	   B	  C		 D		E	   F	  G		 H
-		/*A*/ {	0,	   10,	  9999,  12,	9999,  9999,  11,	 4		},
-		/*B*/ { 9999,  0,	  9999,  8,		9999,  9999,  9999,  20		},
-		/*C*/ { 9999,  9999,  0,	 17,	8,	   9999,  13,	 9999	},
-		/*D*/ { 9999,  9999,  9999,  0,		9999,  16,	  24,	 14		},
-		/*E*/ { 9999,  9999,  9999,  9999,  0,	   8,	  9999,  9999	},
-		/*F*/ { 9999,  9999,  9999,  9999,  9999,  0,	  18,	 2		},
-		/*G*/ { 9999,  9999,  9999,  9999,	11,	   9999,  0,	 3		},
-		/*H*/ { 9999,  9999,  10,	 9999,  5,	   9999,  9999,  0		}
-	};
-
-	vector <pair<int, int>>		currPath;		// Vector of pairs to store the current pathway.
-	vector <pair<int, int>>		fullyExpl;		// Vector of pairs to store the dead end nodes ( Nodes that have already been fully explored. )
-	array  <array<int, 8>, 8>	minKnownDist;	// Array to store the current minimum known distance between nodes.
-
-	int sourceNode  = 0;	// A
-	int destNode    = 1;	// B
-
-	int currNode	= sourceNode;
-
-	toInfinite(minKnownDist);
-
-
-	
-	for (int i = 0; i < V; i++) {
-		for (int j = 0; j < V; j++) {
-			// Still need to check if the node is dead. Are there any child elements or links? when to perform this cehck?
-			if (i == currNode && j == destNode) {	// If the current node has a link to the destination.
-				cout << "FOUND NODE PATH" << endl;
-				cout << graph[i][j] << " Miles away" << endl;
-				currPath.push_back(make_pair(i, j));			// Adds the currently selected node to the current pathway.
-				minKnownDist[i][j] = graph[i][j];				// Sets the index of the new minimum known distance to the discovered distance.
-				// exit from loop or continue running until complete. 
-			}
-			else {
-				if (minKnownDist[i][j] != 9999) {
-					// If the node has already been explored.
-				}
-				//if (find(fullyExpl.begin(), fullyExpl.end(), make_pair(i, j)) {	// Searches through the dead nodes for the current index.
-				//	currPath.push_back(make_pair(i, j));	// Adds current node to current path.
-				//}
-				// check next node // explore children // does node have any children
-
-			}
-		}
-	}
-
-	// Program plan
-	/*
-	
-		Take the source / starting node.
-		Add this to current pathway
-
-		Check if a direct path from the starting / current node to the goal node exists.
-			
-		If it doesn't then:
-			ITERATOR - Iterate through all given indexes of the graph (nodes) {
-
-				check nearest link / next node with the shortest distance from the current node.
-				add this node to the current path
-				check for links to other nodes.
-
-				If no links to other nodes:
-					add index to dead path / fullyExpl vector
-				If there are links to other nodes:
-			}
-
-		Else if it does then:
-			Add to currPath vector and return complete path to be output.
-
-
-	
-	*/
-
+    for (int k = 0; k < v; k++)          // iterates through the verticies.
+    {
+        if (visited[k] == false && dist[k] <= min)  // Checks if the node hasn't been visited yet and if the current path is shorter than the minimum known path ( which is infinite on the initial iteration ).
+                                                    // This is used to find path length to each vertex.
+        {
+            min = dist[k];              // Sets the min value to the current index of min known distance. ( ready for comparison in the next iteration of the for loop )
+            ind = k;                    // Sets the index value to the current index of the iterator / for loop so this can be returned back as the shortest path index for this node/edge.
+        }
+    }
+    return ind;                         // Returns index for shortest identified path.
 }
 
-void toInfinite(array <array<int, V>, V> & minKnownDist) {
-	// Loop to set all minKnownDistances to 9999.
+void RunAlgorithm(int graph[v][v], int src) // adjacency matrix     // Outputs all of the shortest paths from the source node to every other node.
+{
+    int dist[v]; // array to store the minimum path length for each node.                           
+    bool visited[v];// Boolean array to store visited or unvisited for each node.
 
-	for (int i = 0; i < V; i++) {
-		for (int j = 0; j < V; j++) {
-			minKnownDist[i][j] = 9999;
-		}
-	}
 
-	// ==========================================
+    for (int k = 0; k < v; k++) // Iterator to run through each index of the arrays.
+    {
+        dist[k] = 10000;  // Assumes the distance is infinite and assigns value to each index.      ( Initialisation )
+        visited[k] = false;        // False values assigned to all indexes to store unvisited for each node. ( Initialisation )
+    }
+
+    dist[src] = 0;   // Sets the distance of the source node to 0 as this is the only known path length.
+
+    for (int k = 0; k < v; k++) // Iterates through all of the indexes of the arrays.
+    {
+        int m = miniDist(dist, visited);   // Sets m to the calculated minimum distance from each node
+        visited[m] = true;                     // Sets the current node to explored
+        for (int k = 0; k < v; k++)
+        {
+            // updating the distance of neighbouring vertex
+            if (!visited[k] && graph[m][k] && dist[m] != 10000 && dist[m] + graph[m][k] < dist[k])   // Checks if the the current node has not yet been visited. Checks if the cost for this edge is less than the currently known shortest path
+                dist[k] = dist[m] + graph[m][k];                                                       // Updates the shortest known path from the source to the goal node, Adds previous total pathlength to newly discovered node.
+        }
+    }
+    cout << "Vertex\t\t\tDistance from source\n" << endl;        // Titles for output
+    for (int k = 0; k < v; k++)                             // Iterate through saved list.
+    {
+        char str = 65 + k;                                  // Translates numerical values into letters
+        if (dist[k] != 10000) {                             // Checks if the path exists.
+            cout << str << "\t\t\t" << dist[k] << endl;     // Outputs results.( Shortest Path Length)
+        }
+        else {
+            cout << str << "\t\t\t" << "PATH DOESN'T EXIST" << endl;
+        }
+    }
+}
+
+int main()
+{
+    int graph[v][v] = {
+
+        // Cost matrix showing edge cost from vertex [LEFT V] to - [TOP V]
+        //		A	B	C	D	 E	 F	 G	 H
+        /*A*/ {	0,  10, 0,  12,  0,	 0,  11, 4	},
+        /*B*/ { 0,  0,  0,	8,	 0,	 0,	 0,	 20	},
+        /*C*/ { 0,  0,  0,  17,	 8,  0,  13, 0	},
+        /*D*/ { 0,  0,  0,  0,	 0,  16, 24, 14	},
+        /*E*/ { 0,  0,  0,  0,   0,	 8,	 0,  0	},
+        /*F*/ { 0,  0,  0,  0,   0,  0,	 18, 2	},
+        /*G*/ { 0,  0,  0,  0,   11, 0,  0,	 3	},
+        /*H*/ { 0,  0,  10, 0,   5,	 0,  0,  0	}
+    };
+    RunAlgorithm(graph, 5);         // Calls the algorithm funciton with the source node as a parameter.
+    return 0;
 }
