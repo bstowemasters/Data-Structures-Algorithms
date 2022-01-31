5465ebf2132e4009ef56b93d5039ac9792bbed98
bstowemasters <benjamin.stowe-masters20@bathspa.ac.uk>
2022-01-31 12:01:25 +0000

    final changes

--- Code Snippets/Final Algorithm/Final Algorithm/Source.cpp
+++ Code Snippets/Final Algorithm/Final Algorithm/Source.cpp
@@ -5,8 +5,12 @@
 #include <ctime> 
 using namespace std;
 
-#define v 80
-int amount = 0; // used for simulation
+#define v 16
+
+int numOfNodes = 0;
+int numOfEdges = 0;
+
+
 
 
 int miniDist(int dist[], bool visited[]) // finding minimum distance
@@ -15,12 +19,10 @@ int miniDist(int dist[], bool visited[]) // finding minimum distance
 
     for (int k = 0; k < v; k++)          // iterates through the verticies.
     {
-        amount++;
-
         if (visited[k] == false && dist[k] <= min)  // Checks if the node hasn't been visited yet and if the current path is shorter than the minimum known path ( which is infinite on the initial iteration ).
                                                     // This is used to find path length to each vertex.
         {
-            amount++;
+            numOfNodes++;
             min = dist[k];              // Sets the min value to the current index of min known distance. ( ready for comparison in the next iteration of the for loop )
             ind = k;                    // Sets the index value to the current index of the iterator / for loop so this can be returned back as the shortest path index for this node/edge.
         }
@@ -45,15 +47,15 @@ void RunAlgorithm(int graph[v][v], int src) // adjacency matrix     // Outputs a
 
     for (int k = 0; k < v; k++) // Iterates through all of the indexes of the arrays.
     {
-        amount++;
         int m = miniDist(dist, visited);   // Sets m to the calculated minimum distance from each node
         visited[m] = true;                     // Sets the current node to explored
         for (int k = 0; k < v; k++)
         {
-            amount++;
+
             // updating the distance of neighbouring vertex
             if (!visited[k] && graph[m][k] && dist[m] != 10000 && dist[m] + graph[m][k] < dist[k])   // Checks if the the current node has not yet been visited. Checks if the cost for this edge is less than the currently known shortest path
                 dist[k] = dist[m] + graph[m][k];                                                       // Updates the shortest known path from the source to the goal node, Adds previous total pathlength to newly discovered node.
+                numOfEdges++;
         }
     }
     //auto end = std::chrono::system_clock::now();
@@ -71,7 +73,8 @@ void RunAlgorithm(int graph[v][v], int src) // adjacency matrix     // Outputs a
             cout << str << "\t\t\tPATH DOESN'T EXIST" << endl;
         }
     }
-    cout << to_string(amount) << " number of checks performed";
+    cout << endl << "Number of nodes visited:\t" << numOfNodes << endl;
+    cout << "Number of edges visited:\t" << numOfEdges << endl;
     //cout << "\nfinished computation at " << time(&end_time) << "\nelapsed time: " << elapsed_seconds.count() << "s\n";    // Used for testing time of completion.
 }
 
@@ -81,14 +84,23 @@ int main()
 
         // Cost matrix showing edge cost from vertex [LEFT V] to - [TOP V]
         //		A	B	C	D	 E	 F	 G	 H
-        /*A*/ {	0,  10, 0,  12,  0,	 0,  11, 4	},
-        /*B*/ { 0,  0,  0,	8,	 0,	 0,	 0,	 20	},
-        /*C*/ { 0,  0,  0,  17,	 8,  0,  13, 0	},
-        /*D*/ { 0,  0,  0,  0,	 0,  16, 24, 14	},
-        /*E*/ { 0,  0,  0,  0,   0,	 8,	 0,  0	},
-        /*F*/ { 0,  0,  0,  0,   0,  0,	 18, 2	},
-        /*G*/ { 0,  0,  0,  0,   11, 0,  0,	 30	},
-        /*H*/ { 0,  0,  10, 0,   5,	 0,  0,  0	}
+        /*A*/ {	0,  10, 0,  12,  0,	 0,  11, 4,  0,  10, 0,  12,  0,  0,  11, 4	 },
+        /*B*/ { 0,  0,  0,	8,	 0,	 0,	 0,	 20, 0,  0,  0,	 8,	  0,  0,  0,  20 },
+        /*C*/ { 0,  0,  0,  17,	 8,  0,  13, 0,  0,  0,  0,  17,  8,  0,  13, 0	 },
+        /*D*/ { 0,  0,  0,  0,	 0,  16, 24, 14, 0,  0,  0,  0,	  0,  16, 24, 14 },
+        /*E*/ { 0,  0,  0,  0,   0,	 8,	 0,  0,  0,  0,  0,  0,   0,  8,  0,  0	 },
+        /*F*/ { 0,  0,  0,  0,   0,  0,	 18, 2,  0,  0,  0,  0,   0,  0,  18, 2	 },
+        /*G*/ { 0,  0,  0,  0,   11, 0,  0,	 30, 0,  0,  0,  0,   11, 0,  0,  30 },
+        /*H*/ { 0,  0,  10, 0,   5,	 0,  0,  0,  0,  0,  10, 0,   5,  0,  0,  0	 },
+        /*I*/ {	0,  10, 0,  12,  0,	 0,  11, 4,  0,  10, 0,  12,  0,  0,  11, 4  },
+        /*J*/ { 0,  0,  0,	8,	 0,	 0,	 0,	 20, 0,  0,  0,	 8,	  0,  0,  0,  20 },
+        /*K*/ { 0,  0,  0,  17,	 8,  0,  13, 0,  0,  0,  0,  17,  8,  0,  13, 0	 },
+        /*L*/ { 0,  0,  0,  0,	 0,  16, 24, 14, 0,  0,  0,  0,	  0,  16, 24, 14 },
+        /*M*/ { 0,  0,  0,  0,   0,	 8,	 0,  0,  0,  0,  0,  0,   0,  8,  0,  0	},
+        /*N*/ { 0,  0,  0,  0,   0,  0,	 18, 2,  0,  0,  0,  0,   0,  0,  18, 2	},
+        /*O*/ { 0,  0,  0,  0,   11, 0,  0,	 30, 0,  0,  0,  0,   11, 0,  0, 30	},
+        /*P*/ { 0,  0,  10, 0,   5,	 0,  0,  0,  0,  0,  10, 0,   5,  0,  0,  0	}
+
 
     };
 
